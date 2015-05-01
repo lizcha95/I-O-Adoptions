c998a06595ee1816e3c77ca9ad5148b55af65ddb
Ariel Herrera <herrerafariel.345@gmail.com>
2015-05-01 17:31:24 -0600

    ViewPersonas

--- Scripts Proyecto/Views/view_personas.sql
+++ Scripts Proyecto/Views/view_personas.sql
@@ -1,9 +1,8 @@
 create or replace view view_all_data_personas as
-  select p.cedula_persona, p.nombre, p.apellido, p.fecha_nacimiento, u.username, 
-         tu.tipo_usuario, t.numero_telefono, vd.nombre_pais, vd.nombre_provincia, 
-         vd.nombre_canton, vd.direccion_exacta
-  from adoptante a, persona p, usuarios u, telefono t, tipo_usuario tu,
-       view_direcciones vd
-  where a.id_adoptante = p.id_persona and p.usuario_username = u.username and 
-        p.id_persona = t.id_persona and p.tipo_usuario = tu.id_tipo_usuario and 
-        vd.id_direccion = p.id_direccion;
+select p.cedula_persona, p.nombre, p.apellido, p.fecha_nacimiento, u.username,
+         c.correo, t.numero_telefono, vd.nombre_pais, vd.nombre_provincia,
+         vd.nombre_canton, vd.direccion_exacta, a.notas_adoptante
+  from adoptante a, persona p, usuarios u, telefono t, correo c, view_direcciones vd
+  where a.id_adoptante = p.id_persona and p.usuario_username = u.username and
+        p.id_persona = t.id_persona and vd.id_direccion = p.id_direccion
+        and p.id_persona = c.id_persona;
