b61666e5f96ad785b922e10686c554a36d5f750d
David Palacios <gato_palacios_6-1@hotmail.com>
2015-05-01 12:54:31 -0600

    Gets para combo_box David

---
+++ modelo/php/get_nivel_energias.php
@@ -0,0 +1,24 @@
+<?php
+
+$conn = oci_connect('User1', 'User1', 'PROYECTOUNO');
+
+
+
+$stid = oci_parse($conn, 'select * from TABLE(get_nivel_energias())');
+oci_execute($stid);
+
+$nivel_energias = array();
+
+while ($col = oci_fetch_array($stid)) {
+    $idd = strval($col[0]);
+    $nombre = utf8_encode($col[1]);
+    $columna = array("id_nivel_energia" => $idd, "nivel_energia" => $nombre);
+    $nivel_energias[] = $columna;
+
+}
+
+echo json_encode($nivel_energias, JSON_UNESCAPED_UNICODE);
+oci_free_statement($stid);
+oci_close($conn);
+
+?>
